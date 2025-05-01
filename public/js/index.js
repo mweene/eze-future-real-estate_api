3f6b26ea660cc492764a1f96519d51311a80c2d4
mweene <harrisonmweene11@gmail.com>
2025-05-01 20:01:06 +0200

    Deleted all files,fresh start

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const menuButton = document.querySelector("#nav_menu");
-
-menuButton.addEventListener("click", () => {
-  document.querySelector(".nav_list").classList.toggle("hidden");
-});
